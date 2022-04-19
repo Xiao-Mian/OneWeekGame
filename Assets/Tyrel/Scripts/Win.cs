5425548de6299c901df0e0b4b4fa1fe211f4047c
xyndall <tyrel.ratima@gmail.com>
2022-04-19 12:04:47 +1200

    Scripts in my folder
    just copy what i have but learn what it does and how it works, ill help

---
+++ Assets/Tyrel/Scripts/Win.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    public Text _youWin;
+    public Timer _timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        _youWin.enabled = false;
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            _timer.EndTimer();
+            _youWin.enabled = true;
+            
+            StartCoroutine(Restart());
+        }
+    }
+
+
+    IEnumerator Restart()
+    {
+        yield return  new WaitForSeconds(2f);
+        Time.timeScale = 1;
+        
+        SceneManager.LoadScene(1);
+
+        
+    }
+
+}
